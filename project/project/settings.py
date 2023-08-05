34b21a70cbde83c29f3ab520c3991714ecae0b38
egnaro1007 <ndviettuan@gmail.com>
2023-08-05 15:07:11 +0700

    Allow all origins

--- project/project/settings.py
+++ project/project/settings.py
@@ -174,6 +174,7 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5173',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:5173',
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
