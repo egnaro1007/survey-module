179fb29d83bbdc8d3d839dfa4add502637fd2d14
xuananle <lexuanana18102004@gmail.com>
2023-08-05 17:23:53 +0700

    Adding style 2

--- front-end/src/routes/survey/[surveyID]/+page.svelte
+++ front-end/src/routes/survey/[surveyID]/+page.svelte
@@ -1,23 +1,52 @@
 <script>
-    import { page } from "$app/stores";
-    import { onMount } from "svelte";
-    import {writable} from "svelte/store";
-
-    let survey_res = writable(null);
-
+    import {page} from "$app/stores";
+    import {onMount} from "svelte";
+    let Survey_res;
     onMount(async () => {
         const survey_id = $page.params.surveyID;
-        const response = await fetch('http://192.168.2.18:8000/survey/api/survey?survey_id='+survey_id);
-        survey_res = await response.json();
+        const response = await fetch(
+            "http://192.168.2.18:8000/survey/api/survey?survey_id=" + survey_id
+        );
+        if (response.ok) {
+            Survey_res = await response.json();
+        }
     });
 </script>
 
-<main>
-    <h1>{survey_res.title}</h1>
-    <p>{survey_res.description}</p>
+{#if Survey_res}
+    <h1>{Survey_res.title}</h1>
+    <p>{Survey_res.description}</p>
+
     <ul>
-        {#each survey_res.questions as ques}
-            <p>hello</p>
+        {#each Survey_res.questions as ques}
+            <h2>{ques.text}</h2>
+            {#if ques.type === "multi"}
+                <ul>
+                    {#each ques.choices as item}
+                        <li>
+                            <label>
+                                <input type="checkbox" name="{ques.id}" value="{item.id}">
+                                {item.text}
+                            </label>
+                        </li>
+                    {/each}
+                </ul>
+            {:else if ques.type === "single"}
+                <ul>
+                    {#each ques.choices as item}
+                        <li>
+                            <label>
+                                <input type="radio" name="{ques.id}" value="{item.id}">
+                                {item.text}
+                            </label>
+                        </li>
+                    {/each}
+                </ul>
+            {:else}
+                <input type="text" name="{ques.id}">
+            {/if}
         {/each}
     </ul>
-</main>
+{:else}
+    <p>...waiting</p>
+{/if}
