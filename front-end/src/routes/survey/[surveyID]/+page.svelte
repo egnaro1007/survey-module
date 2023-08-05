d5cf5386b540f1533a7c6470054bfa4545848bc3
xuananle <lexuanana18102004@gmail.com>
2023-08-05 18:09:33 +0700

    Adding Style 3

--- front-end/src/routes/survey/[surveyID]/+page.svelte
+++ front-end/src/routes/survey/[surveyID]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import {page} from "$app/stores";
     import {onMount} from "svelte";
+
     let Survey_res;
     onMount(async () => {
         const survey_id = $page.params.surveyID;
@@ -13,40 +14,52 @@
     });
 </script>
 
-{#if Survey_res}
-    <h1>{Survey_res.title}</h1>
-    <p>{Survey_res.description}</p>
-
-    <ul>
+<div class="container">
+    {#if Survey_res}
+        <h1>{Survey_res.title}</h1>
+        <p>{Survey_res.description}</p>
         {#each Survey_res.questions as ques}
-            <h2>{ques.text}</h2>
-            {#if ques.type === "multi"}
-                <ul>
-                    {#each ques.choices as item}
-                        <li>
-                            <label>
-                                <input type="checkbox" name="{ques.id}" value="{item.id}">
-                                {item.text}
-                            </label>
-                        </li>
-                    {/each}
-                </ul>
-            {:else if ques.type === "single"}
-                <ul>
-                    {#each ques.choices as item}
-                        <li>
-                            <label>
-                                <input type="radio" name="{ques.id}" value="{item.id}">
-                                {item.text}
-                            </label>
-                        </li>
-                    {/each}
-                </ul>
-            {:else}
-                <input type="text" name="{ques.id}">
-            {/if}
+            <li class="my-3">
+                <h2>{ques.text}</h2>
+                {#if ques.type === "multi"}
+                    <ul class="list-unstyled">
+                        {#each ques.choices as item}
+                            <li>
+                                <label>
+                                    <input type="checkbox" name="{ques.id}" value="{item.id}" class="mr-2">
+                                    {item.text}
+                                </label>
+                            </li>
+                        {/each}
+                    </ul>
+                {:else if ques.type === "single"}
+                    <ul class="list-unstyled">
+                        {#each ques.choices as item}
+                            <li>
+                                <label>
+                                    <input type="radio" name="{ques.id}" value="{item.id}" class="mr-2">
+                                    {item.text}
+                                </label>
+                            </li>
+                        {/each}
+                    </ul>
+                {:else}
+                    <input type="text" name="{ques.id}" class="form-control">
+                {/if}
+            </li>
         {/each}
-    </ul>
-{:else}
-    <p>...waiting</p>
-{/if}
+    {:else}
+        <p>...waiting</p>
+    {/if}
+    <input type="submit" class="btn btn-success" placeholder="Submit Form" id = "xuanan">
+
+</div>
+
+<style>
+    .btn, .btn-success {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+</style>
