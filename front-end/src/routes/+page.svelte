c4e8453a528e73b1bc15137ba3ef542a8c69871a
egnaro1007 <ndviettuan@gmail.com>
2023-08-06 10:15:08 +0700

    dark mode

--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,3 +1,21 @@
-<a href="survey">Click here to server</a>
-<br>
-<a href = "Login">Login</a>
+<!--<a href="survey">Click here to server</a>-->
+<!--<br>-->
+<!--<a href = "Login">Login</a>-->
+<nav class="navbar navbar-expand-lg">
+
+    <div class="container-fluid">
+        <!-- Links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="survey">Survey</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Login">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link 3</a>
+            </li>
+        </ul>
+    </div>
+
+</nav>
